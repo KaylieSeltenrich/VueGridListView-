2bef7b3033b1556c0b5eebf433bd1bf6f8c3a0de
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-09-18 01:19:04 -0600

    Finished Week13 Vue Grid List Assignment

--- src/components/Tweets.vue
+++ src/components/Tweets.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="container" :class="{ grid: isGrid, list: isList }">
-    <button id="toggle" type="button"> Click here to change view </button>
+  <div>
+   <button id="toggle" type="button" @click="isList = !isList, isGrid = !isGrid"> Click Here To Change View! </button>
+    <div id="container" :class="{ grid: isGrid, list: isList }">
     <div id="tweet-container" v-for="allTweet in allTweets" v-bind:key="allTweet"> 
     <h3 id="user-tweet"> {{ "'"+ allTweet.userTweet + "'" }} </h3>
     <h3 id="user-name"> {{ allTweet.userName }}  </h3>
     <h3 id="date-created"> {{ allTweet.dateCreated }} </h3>
+    </div>
      </div>
   </div>
 </template>
@@ -12,8 +14,7 @@
 <script>
 
 export default {
-  
-
+     
  data: function() {
 
     return {
@@ -24,12 +25,12 @@ export default {
 
          { userTweet: "Hello World!",
           userName: "CoolUser",
-          dateCreated: "9/17/2020 ",
+          dateCreated: "9/18/2020 ",
           },
 
         { userTweet: "I can't wait for the weekend.",
           userName: "IHateMondays",
-          dateCreated: "9/17/2020",
+          dateCreated: "9/18/2020",
           },
 
         { userTweet: "I really like dogs.",
@@ -44,12 +45,20 @@ export default {
 
         { userTweet: "WEEEE!",
           userName: "CaffinatedCoder",
-          dateCreated: "9/17/2020",
+          dateCreated: "9/16/2020",
           },
+
+           { userTweet: "Tweet Tweet",
+          userName: "Birb",
+          dateCreated: "9/15/2020 ",
+          },
+
           
        
         ],
        
+    isGrid: false,
+    isList: true,
 }
 }
 }
@@ -59,7 +68,7 @@ export default {
 
 <style>
 #user-tweet {
-    font-size: 1.5em;
+    font-size: 2em;
     margin: 5%;
     color: grey;
 }
@@ -67,6 +76,8 @@ export default {
 #tweet-container {
     border: 2px solid black; 
     margin: 5%;
+    padding: 5%;
+    width: 80%;
 }
 
 .grid {
@@ -74,13 +85,36 @@ export default {
 display: grid;
 justify-items: center;
 align-items: center;
-grid-template-columns: repeat(2,1fr);
+grid-template-columns: 1fr 1fr;
+
+
 
 }
 
 .list {
     display: block;
     text-align: center;
+    padding: 2%;
+    
+   
+  
+}
+
+.list #user-tweet {
+  font-size: 3em;
+  
+}
+
+.list #user-name, #date-created {
+  font-size: 2em;
+  
+}
+
+#toggle {
+  
+  width: 80%;
+  height: 10%;
+  font-size: 1.5em;
 }
 
 </style>
